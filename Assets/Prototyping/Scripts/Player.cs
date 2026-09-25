9d338eff16bb8b47bf124fb36aa291eea5586f08
InfiniteClock <bryster97@hotmail.com>
2026-09-25 15:56:04 -0400

    Movement Prototype
    - Player can now move around using WASD or Left Joystick
    - Player has speed applied in set direction
    - Ignores vertical speeds (will address this in jumping addition later)
    - Ground Check is made to determine if player is touching ground and applies drag if so, no drag if airborne

--- Assets/Prototyping/Scripts/Player.cs
+++ Assets/Prototyping/Scripts/Player.cs
@@ -1,24 +1,44 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Guns")]
     public Camera playerCam;
     public Weapon rightWeapon;
     public Weapon leftWeapon;
-
     private InputAction rightFireInput;
     private InputAction leftFireInput;
+
+    [Header("Movement")]
+    public float baseMoveSpeed;
+    public float groundDrag;
+    //public float baseAccelTime;
+    private Vector3 inputDirection;
+    private InputAction moveInput;
+    private Rigidbody rb;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+    
     private void OnEnable()
     {
         rightFireInput = InputSystem.actions.FindAction("Right Fire");
         leftFireInput = InputSystem.actions.FindAction("Left Fire");
+
+        moveInput = InputSystem.actions.FindAction("Move");
 
         // Interaction = Press Only, Press Point = 1
         rightFireInput.performed += RightCharge;
         rightFireInput.canceled += RightFire;
         leftFireInput.performed += LeftCharge;
         leftFireInput.canceled += LeftFire;
+
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
     private void OnDisable()
     {
@@ -27,14 +47,55 @@ public class Player : MonoBehaviour
         leftFireInput.performed -= LeftCharge;
         leftFireInput.canceled -= LeftFire;
     }
+
     private void Update()
     {
-        Debug.DrawRay(rightWeapon.transform.position, playerCam.transform.forward*100f, Color.cyan);
-        Debug.DrawRay(leftWeapon.transform.position, playerCam.transform.forward*100f, Color.cyan);
-    }
-    private void RightCharge(InputAction.CallbackContext context) => rightWeapon.Charge(); //rightWeapon.Charge();
-    private void LeftCharge(InputAction.CallbackContext context) => leftWeapon.Charge(); //leftWeapon.Charge();
-    private void RightFire(InputAction.CallbackContext context) => rightWeapon.Shoot(); //rightWeapon.Shoot();
-    private void LeftFire(InputAction.CallbackContext context) => leftWeapon.Shoot(); //leftWeapon.Shoot();
+        // Ground Check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
+        // Draw rays from player guns for debugging direciton
+        Debug.DrawRay(rightWeapon.transform.position, playerCam.transform.forward*20f, Color.cyan);
+        Debug.DrawRay(leftWeapon.transform.position, playerCam.transform.forward*20f, Color.cyan);
+
+
+        // Player motion
+        Move();
+        SpeedControl();
+
+        // Apply Drag
+        if (isGrounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+
+        //Debug.Log("Player speed = " + rb.linearVelocity.magnitude);
+    }
+    private void Move()
+    {
+        // Gets the normalized direction vector2 from player input
+        Vector2 movement = moveInput.ReadValue<Vector2>();
+
+        inputDirection = playerCam.transform.forward * movement.y + playerCam.transform.right * movement.x;
+
+        rb.AddForce(inputDirection.normalized * baseMoveSpeed, ForceMode.Force);
+        
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        // Prevents player from moving faster than movespeed in x and z directions while ignoring y speed
+        if (flatVelocity.magnitude > baseMoveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * baseMoveSpeed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+        }
+
+        
+    }
+    private void RightCharge(InputAction.CallbackContext context) => rightWeapon.Charge();
+    private void LeftCharge(InputAction.CallbackContext context) => leftWeapon.Charge();
+    private void RightFire(InputAction.CallbackContext context) => rightWeapon.Shoot(); 
+    private void LeftFire(InputAction.CallbackContext context) => leftWeapon.Shoot(); 
+    
 }
